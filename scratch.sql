189c1a2185669be63c35140fa2710bd75a51778c
Derek <derlloyd@outlook.com>
2016-04-26 18:51:38 +0000

    exercise3

---
+++ scratch.sql
@@ -0,0 +1,16 @@
+
+mysql-ctl start
+mysql -u derlloyd
+create database decodemtl_addressbook;
+exit back to command line
+
+$ mysql -u derlloyd decodemtl_addressbook < ./data/import-table-structure.sql
+
+INSERT INTO Account (email, password) VALUES ("buddy@gmail.com", "buddy123");
+INSERT INTO AddressBook (accountId, name) VALUES (1, "gmail addressbook");
+
+INSERT INTO Entry (addressBookId, firstName, lastName, birthday, type, subtype, contentLineOne) 
+VALUES 
+    (1, "John", "Smith", "1/1/2000", "home", "phone", "514-555-1212"),
+    (1, "Chris", "Adams", "12/15/1980", "work", "address", "12 Pine Ave"),
+    (1, "Terry", "Oreily", "5/28/1990", "work", "email", "terry@yahoo.com");
